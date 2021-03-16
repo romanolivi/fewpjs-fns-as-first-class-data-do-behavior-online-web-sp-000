34fb8e6bbbc1dbd45375a233afc15b9c8616e0d7
romanolivi <romano_livi@outlook.com>
2021-03-16 12:17:30 -0400

    commit tree 2225d399560d823aba4334769e0f7db95b59e32b

--- index.js
+++ index.js
@@ -9,5 +9,24 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(timeString) {
+  const hoursMinutes = timeString.split(':');
+  const hours = parseInt(hoursMinutes[0], 10);
+  const minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
+  const time = (hours + minutes / 100);
+
+  if (time < 12) { 
+    greeting = "Good Morning";
+  } else if (time > 17) { 
+    greeting = "Good Evening";
+  } else if (12 < time < 17) {
+    greeting = "Good Afternoon";
+  }
+
+  return greeting;
+}
+
+function displayMessage(b) {
+  const g = document.getElementById("greeting");
+  g.innerText = b;
+} 
